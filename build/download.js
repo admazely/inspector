199d55ffd9059707a6df3f715e9171552a1a3688
Andreas Madsen <amwebdk@gmail.com>
2012-11-13 22:06:19 +0100

    [fix] use url pointing to latest inspector.json

--- build/download.js
+++ build/download.js
@@ -1,8 +1,8 @@
 
-var http = require('http');
+var https = require('https');
 var path = require('path');
 var fs = require('fs');
 
-http.get('http://trac.webkit.org/export/129139/trunk/Source/WebCore/inspector/Inspector.json', function (res) {
+https.get('https://raw.github.com/WebKit/webkit/master/Source/WebCore/inspector/Inspector.json', function (res) {
     res.pipe( fs.createWriteStream(path.resolve(__dirname, 'inspector.json')) );
 });
